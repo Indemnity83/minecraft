524069fc8bb4817761f350962bb05a97029dc276
Kyle Klaus <kklaus@indemnity83.com>
2020-05-02 00:02:34 -0700

    operate as unprivileged user

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,44 @@
 FROM openjdk:15-jdk-alpine
 LABEL maintainer="Kyle Klaus <kklaus@indemnity83.com>"
 
+# Minecraft Version
 ENV MC_VERSION=latest
 
+# Java parameters
 ENV JAVA_XMX=1024m
 ENV JAVA_XMS=1024m
 ENV JAVA_ARGS=
 
+# Jar parameters
 ENV JAR_FILE=minecraft_server.jar
 ENV JAR_ARGS=
 
+# User/Group IDs
+ARG UID=99
+ARG GID=100
+
 # Install packages
-RUN apk --no-cache add jq wget
+RUN apk --no-cache add jq wget shadow
 
+# Copy the init script into the container
 COPY init.sh /bin/init
-RUN mkdir -p /minecraft
 
+# Setup an unprivlidged user
+RUN useradd -r -u $UID -o minecraft && \
+    groupmod -g $GID -o minecraft
+
+# Setup the minecraft volume
+RUN mkdir -p /minecraft && \
+    chown minecraft:minecraft /minecraft
+
+# Run as unprivlidged user in default path
+WORKDIR /minecraft
+USER minecraft
+
+# Expose the application surface
 VOLUME /minecraft
 EXPOSE 25565
 EXPOSE 25575
 
-WORKDIR /minecraft
+# Start the server
 CMD /bin/init
--- init.sh
+++ init.sh
@@ -2,29 +2,28 @@
 java -version
 
 if [ ! -f "minecraft_server.jar" ]; then
-    ## Get mojang version manifest
+    ## Get Mojang's version manifest
     wget -q --show-progress -O version_manifest.json -r https://launchermeta.mojang.com/mc/game/version_manifest.json
 
-    ## If version isn't set, we need to set it to the latest version
+    ## If version isn't set, or is set to 'latest' we need to set it to the actual version number
     if [ -z "${MC_VERSION}" ] || [ "$MC_VERSION" == "latest" ]; then
         MC_VERSION=`jq -r '.latest.release' version_manifest.json`
     fi
 
-    ## Get the game version manifest
+    ## Get the game version manifest 
     wget -q --show-progress -O $MC_VERSION.json `jq -r '.versions | .[] | select(.id=="'${MC_VERSION}'") | .url' version_manifest.json`
 
     ## Download and check the minecraft server jar
     wget -q --show-progress -O $JAR_FILE `jq -r '.downloads.server.url' ${MC_VERSION}.json`
 
-    ## Get the server hash file
+    ## Get the hash
     echo "`jq -r '.downloads.server.sha1' ${MC_VERSION}.json`  minecraft_server.jar" > $JAR_FILE.sha1
 fi
 
-## Check the minecraft server file is good
+## Verify the jar file if we can
 if [ -f $JAR_FILE.sha1 ]; then 
     sha1sum -c $JAR_FILE.sha1
 fi
 
-## Launch minecraft in a screen
+## Launch minecraft straight up
 java -Xmx$JAVA_XMX -Xms$JAVA_XMS $JAVA_ARGS -jar $JAR_FILE nogui $JAR_ARGS
-
